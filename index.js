3694d031c65795e1c7f04e11b7784aea7e2a8e2b
sabb12 <sabb0923@gmail.com>
2024-07-01 17:08:45 +0900

    css 수정 및 반응형 적용

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ function listToHtml(list) {
 
 function todoListHTML(todo) {
   return `<div class="todoContainer" data-id="${todo.id}" >
+            <div class="todoSection1">
               <input type="checkbox" class="checkboxValue" ${
                 todo.checked ? "checked" : ""
               }/>
-                <input type="text" class="todoContent" value="${
-                  todo.content
-                }" readonly/>
+              <input type="text" class="todoContent" value="${
+                todo.content
+              }" readonly/>
+            </div>
+            <div class="todoSection2">
               ${
                 todo.checked ? "" : '<button class="updateButton">수정</button>'
               }
-                <button class="deleteButton">삭제</button>
-            </div>`;
+              <button class="deleteButton">삭제</button>
+            </div>
+          </div>`;
 }
 
 // todo 추가
@@ -52,34 +56,40 @@ document.getElementById("addBtn").addEventListener("click", todoItem);
 // 삭제
 document.addEventListener("click", function deleteButton(e) {
   if (!e.target.classList.contains("deleteButton")) return;
-  const id = Number(e.target.parentElement.dataset.id);
+  const todoContainer = e.target.closest(".todoContainer");
+  const id = Number(todoContainer.dataset.id);
 
   // 배열에서만 지워지고 있다
   newArray = newArray.filter((item) => item.id !== id);
   // DOM에서 지운다
-  e.target.parentElement.remove();
+  todoContainer.remove();
 });
 
+// 체크박스
 document.addEventListener("click", function checkBox(e) {
   if (!e.target.classList.contains("checkboxValue")) return;
-  const id = Number(e.target.parentElement.dataset.id);
+  const todoContainer = e.target.closest(".todoContainer");
+  const id = Number(todoContainer.dataset.id);
   const todoItem = newArray.find((item) => item.id === id);
   todoItem.checked = !todoItem.checked;
 
-  e.target.parentElement.remove();
+  todoContainer.remove();
 
-  !todoItem.checked
-    ? document
-        .querySelector(".todoInnerHTML")
-        .insertAdjacentHTML("beforeend", todoListHTML(todoItem))
-    : document
-        .querySelector(".completedInnerHTML")
-        .insertAdjacentHTML("beforeend", todoListHTML(todoItem));
+  if (!todoItem.checked) {
+    document
+      .querySelector(".todoInnerHTML")
+      .insertAdjacentHTML("beforeend", todoListHTML(todoItem));
+  } else {
+    document
+      .querySelector(".completedInnerHTML")
+      .insertAdjacentHTML("beforeend", todoListHTML(todoItem));
+  }
 });
 
+// 수정 버튼
 document.addEventListener("click", function updateButton(e) {
   if (!e.target.classList.contains("updateButton")) return;
-  const todoContainer = e.target.parentElement;
+  const todoContainer = e.target.closest(".todoContainer");
   const todoContent = todoContainer.querySelector(".todoContent");
 
   if (e.target.textContent === "수정") {
